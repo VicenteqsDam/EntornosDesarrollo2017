9d5a89c8b565b1f3d66f7fc3cdcd5af0542ba662
Daniel Pastor Sellers <danipaspastor@gmail.com>
2017-10-19 16:39:03 +0200

    Correccion buenos modales

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -3,18 +3,20 @@
 using namespace std;
 
 int main() {
-	
-	cout << "Hola amigo mio, como te llamas?"<<endl;
 	string nombre; //string en minisculas
-	cin>>nombre;
-	
-	cout<<"\n"<<"Encantado de conocerte "<<nombre<<endl;
-	
-	cout<<"\n"<<"Cuantos anyos tienes "<<nombre<<endl;
 	int edad;
-	cin>>edad;
 	
-	cout<<"\n"<<nombre<<", Dentro de 10 anyos tendras "<<edad+10<<" anyos"<<endl;
-	}
+	cout << "Hola amigo mio, como te llamas?" << endl;
+	
+	cin >> nombre;
+	
+	cout << "\n" << "Encantado de conocerte " << nombre << endl;
+	
+	cout << "\n" <<"Cuantos anyos tienes " << nombre << endl;
+	
+	cin >> edad;
+	
+	cout << "\n" << nombre <<", Dentro de 10 anyos tendras " << edad+10 << " anyos" << endl;
+}
 	
 	
