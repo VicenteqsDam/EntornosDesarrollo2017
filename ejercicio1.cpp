560424ae56d5b568a38bdb950fde3306fbb32853
dedoxys <pepevelezcuadrado@gmail.com>
2017-10-19 15:40:39 +0200

    pepe

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,1 +1,5 @@
-
+int main(int argc, char const *argv[])
+{
+	cout<<"Hola Mundo";
+	return 0;
+}
---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+print ("Bienvenido usuario, cual es vuestro nombre?")
+nombre= input()
+print("Encantado de conocerte, ",nombre," cual es vuestra edad?")
+edad= int(input())
+print("Entonces tendras ",edad+10," en diez anyos")
